df6905f1dcce93deedd8045e194730057940bf80
Dan Boice <db@psycheweb.com>
2012-07-12 10:22:37 -0700

    Added a subclass of scarrayofobjectssection to get the footer totals and notes for track types in the monthly practicum log

---
+++ SCArrayOfObjectsSectionWithTotalAndNotesViewInFooter.h
@@ -0,0 +1,13 @@
+//
+//  SCArrayOfObjectsSectionWithTotalAndNotesViewInFooter.h
+//  PsyTrack
+//
+//  Created by Daniel Boice on 7/12/12.
+//  Copyright (c) 2012 PsycheWeb LLC. All rights reserved.
+//
+
+#import "SCTableViewSection.h"
+
+@interface SCArrayOfObjectsSectionWithTotalAndNotesViewInFooter : SCArrayOfObjectsSection
+
+@end
---
+++ SCArrayOfObjectsSectionWithTotalAndNotesViewInFooter.m
@@ -0,0 +1,13 @@
+//
+//  SCArrayOfObjectsSectionWithTotalAndNotesViewInFooter.m
+//  PsyTrack
+//
+//  Created by Daniel Boice on 7/12/12.
+//  Copyright (c) 2012 PsycheWeb LLC. All rights reserved.
+//
+
+#import "SCArrayOfObjectsSectionWithTotalAndNotesViewInFooter.h"
+
+@implementation SCArrayOfObjectsSectionWithTotalAndNotesViewInFooter
+
+@end
