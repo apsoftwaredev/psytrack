bb646d686a7ba4bf106ce17f60863a39b43bb5a9
Dan Boice <db@psycheweb.com>
2012-07-17 19:09:53 -0700

    working on log generator view controller

---
+++ MonthlyPracticumLogGenerateViewController.h
@@ -0,0 +1,13 @@
+//
+//  MonthlyPracticumLogGenerateViewController.h
+//  PsyTrack
+//
+//  Created by Daniel Boice on 7/17/12.
+//  Copyright (c) 2012 PsycheWeb LLC. All rights reserved.
+//
+
+#import "SCViewController.h"
+
+@interface MonthlyPracticumLogGenerateViewController : SCViewController
+
+@end
