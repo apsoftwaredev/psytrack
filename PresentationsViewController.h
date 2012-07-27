f7296e445635aa13c056556100b630224e305a8d
Dan Boice <db@psycheweb.com>
2012-07-27 06:16:57 -0700

    working on presentation view controller

---
+++ PresentationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TeachingViewController.h
+//  PsyTrack
+//
+//  Created by Daniel Boice on 7/26/12.
+//  Copyright (c) 2012 PsycheWeb LLC. All rights reserved.
+//
+
+#import "SCTableViewController.h"
+
+@interface TeachingViewController : SCTableViewController
+
+@end
---
+++ PresentationsViewController.m
@@ -0,0 +1,44 @@
+//
+//  TeachingViewController.m
+//  PsyTrack
+//
+//  Created by Daniel Boice on 7/26/12.
+//  Copyright (c) 2012 PsycheWeb LLC. All rights reserved.
+//
+
+#import "TeachingViewController.h"
+
+@interface TeachingViewController ()
+
+@end
+
+@implementation TeachingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
