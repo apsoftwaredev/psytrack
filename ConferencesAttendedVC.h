788792566359037b7ebe27cd8522f3bee467c707
Dan Boice <db@psycheweb.com>
2012-08-12 16:32:17 -0700

    added conferences attended vc

---
+++ ConferencesAttendedVC.h
@@ -0,0 +1,13 @@
+//
+//  ConferencesAttendedVC.h
+//  PsyTrack
+//
+//  Created by Daniel Boice on 8/12/12.
+//  Copyright (c) 2012 PsycheWeb LLC. All rights reserved.
+//
+
+#import <SensibleTableView/SensibleTableView.h>
+
+@interface ConferencesAttendedVC : SCTableViewController
+
+@end
