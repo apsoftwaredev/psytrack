4c521c12b1cdf4c48ed4ec61f224d119b13319f2
Dan Boice <db@psycheweb.com>
2012-08-12 18:39:27 -0700

    added teaching experience  vc

---
+++ TeachingExperienceVC.h
@@ -0,0 +1,13 @@
+//
+//  TeachingExperienceVC.h
+//  PsyTrack
+//
+//  Created by Daniel Boice on 8/12/12.
+//  Copyright (c) 2012 PsycheWeb LLC. All rights reserved.
+//
+
+#import <SensibleTableView/SensibleTableView.h>
+
+@interface TeachingExperienceVC : SCTableViewController
+
+@end
