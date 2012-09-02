61607b52c18ca306e588ea1c24cad4a8fc20ec29
Dan Boice <db@psycheweb.com>
2012-09-01 18:47:53 -0700

    added community service vc

---
+++ CommunityServiceVC.h
@@ -0,0 +1,13 @@
+//
+//  CommunityServiceVC.h
+//  PsyTrack
+//
+//  Created by Daniel Boice on 9/1/12.
+//  Copyright (c) 2012 PsycheWeb LLC. All rights reserved.
+//
+
+#import <SensibleTableView/SensibleTableView.h>
+
+@interface CommunityServiceVC : SCTableViewController
+
+@end
