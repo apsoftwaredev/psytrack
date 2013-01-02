b9791eb5c9182f5593fa0eeae5bc65b741f0cf0f
Dan Boice <db@psycheweb.com>
2013-01-01 17:21:34 -0700

    add Pt managed object

---
+++ PTManagedObject.h
@@ -0,0 +1,13 @@
+//
+//  PTManagedObject.h
+//  PsyTrack
+//
+//  Created by Daniel Boice on 1/1/13.
+//  Copyright (c) 2013 PsycheWeb LLC. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface PTManagedObject : NSManagedObject
+
+@end
---
+++ PTManagedObject.m
@@ -0,0 +1,13 @@
+//
+//  PTManagedObject.m
+//  PsyTrack
+//
+//  Created by Daniel Boice on 1/1/13.
+//  Copyright (c) 2013 PsycheWeb LLC. All rights reserved.
+//
+
+#import "PTManagedObject.h"
+
+@implementation PTManagedObject
+
+@end
