e76b6769d609e5b6b37aac8084ca209fd496f681
Dan Boice <db@psycheweb.com>
2013-01-04 06:54:24 -0700

    updated entitlements and worked on core data save bug

---
+++ SCCoreDataStore_SCCoreDataStore_ContextSaveRepair.h
@@ -0,0 +1,13 @@
+//
+//  SCCoreDataStore_SCCoreDataStore_ContextSaveRepair.h
+//  PsyTrack
+//
+//  Created by Daniel Boice on 1/3/13.
+//  Copyright (c) 2013 PsycheWeb LLC. All rights reserved.
+//
+
+#import <STV+CoreData/STV+CoreData.h>
+
+@interface SCCoreDataStore ()
+
+@end
