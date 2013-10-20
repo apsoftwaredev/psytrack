1258dfd7df4a9cba94262ebc46943d4495b6635f
Dan Boice <db@psycheweb.com>
2013-10-19 21:15:13 -0700

    changes for iOS7

---
+++ ValidateMOC.h
@@ -0,0 +1,13 @@
+//
+//  ValidateMOC.h
+//  PsyTrack
+//
+//  Created by Daniel Boice on 10/19/13.
+//  Copyright (c) 2013 PsycheWeb LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ValidateMOC : NSObject
+
+@end
---
+++ ValidateMOC.m
@@ -0,0 +1,13 @@
+//
+//  ValidateMOC.m
+//  PsyTrack
+//
+//  Created by Daniel Boice on 10/19/13.
+//  Copyright (c) 2013 PsycheWeb LLC. All rights reserved.
+//
+
+#import "ValidateMOC.h"
+
+@implementation ValidateMOC
+
+@end
